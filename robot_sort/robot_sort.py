b54a9a6b6665422d6b1737435ece2361ab43f3b9
IVB107 <ianvb107@gmail.com>
2019-06-07 12:33:25 -0500

    Completed robot_sort.py

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,16 +97,57 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
+
         while True:
-            if self.light_is_on():
-                self.set_light_off()
-            print('Light is off')
+            # grab first item
+            if self.compare_item() == None:
+                self.swap_item()
+            self.set_light_off()
+            while self.can_move_right() == True:
+                self.move_right()
+                if self.compare_item() == 1:
+                    # print('Swapping items...')
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    self.set_light_on()
+                # if next item is smaller than held item, move backwards 1 and put item back
+                if self.compare_item() == 0 or self.compare_item() == -1:
+                    # print('Swapping items...')
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    self.swap_item()
+                # Put largest int at end of list
+                if self.can_move_right() == False and self._item != None:
+                    # print('Swapping items...')
+                    self.swap_item()
+            # Move to beginning of list
+            while self._position > 0:
+                self.move_left()
+            # No swaps, list is sorted
+            if self.light_is_on() == False:
+                break
 
-
+    # Bubble sort:
+    # ----------------------
+    # while True:
+    #     swaps = 0
+    #     for i in range(0, len(arr) - 1):
+    #         if arr[i] > arr[i+1]:
+    #             arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    #             swaps += 1
+    #             break
+    #     if swaps == 0:
+    #         break
+    # return arr
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
+
+    # l = [15, 41, 58, 49, 26]
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
